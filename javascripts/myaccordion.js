45c0e0cb962dd5c7fcab68875ed45fbabc5e445d
David Loureiro <david.loureiro@sysfera.com>
2012-10-30 23:30:53 +0100

    Some more bug fixes

--- javascripts/myaccordion.js
+++ javascripts/myaccordion.js
@@ -6,5 +6,27 @@ jQuery().ready(function(){
 		event: 'mouseover',
 		fillSpace: true,
 		animated: 'easeslide'
-	});	
+	});
+	
+	var accordions = jQuery('#navigation');
+		
+		jQuery('#switch select').change(function() {
+			accordions.accordion("activate", this.selectedIndex-1 );
+		});
+		jQuery('#close').click(function() {
+			accordions.accordion("activate", -1);
+		});
+		jQuery('#switch2').change(function() {
+			accordions.accordion("activate", this.value);
+		});
+		jQuery('#enable').click(function() {
+			accordions.accordion("enable");
+		});
+		jQuery('#disable').click(function() {
+			accordions.accordion("disable");
+		});
+		jQuery('#remove').click(function() {
+			accordions.accordion("destroy");
+			wizardButtons.unbind("click");
+		});
 });
